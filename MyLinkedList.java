e4513253b79993c13c346fedd4e648e898f9872a
Andrew <ajuang20@stuy.edu>
2021-01-06 13:12:47 -0500

    Added size() method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,7 +5,9 @@ public class MyLinkedList{
     public MyLinkedList(){
         size = 0;
     }
-    public int size();
+    public int size(){
+        return size;
+    }
     public boolean add(String value);
     public boolean add(int index, String value);
     public String get(int index);
