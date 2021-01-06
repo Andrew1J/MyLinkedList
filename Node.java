4e80615e419f61a271e2db81903a304e93530866
Andrew <ajuang20@stuy.edu>
2021-01-06 13:11:21 -0500

    added constructors and get/set

--- Node.java
+++ Node.java
@@ -1,6 +1,32 @@
 public class Node{
-    public Node(String value){/*create a constructor*/}
     private String data;
     private Node next,prev;
-    //write get/set methods for all three instance variables.
+    
+    public Node(String value){
+        data = value;
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public void getData(){
+        return data;
+    }
+
+    public void setNext(Node next){
+        this.next = next;
+    }
+
+    public void getNext(){
+        return next;
+    }
+
+    public void setPrev(Node prev){
+        this.prev = prev;
+    }
+
+    public void getPrev(){
+        return prev;
+    }
 }
