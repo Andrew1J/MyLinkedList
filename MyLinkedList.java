89a08d36b9dfffa6d29f1a8689e920dfce4d42a0
Andrew <ajuang20@stuy.edu>
2021-01-06 20:05:24 -0500

    Added private helper method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,9 +5,11 @@ public class MyLinkedList{
     public MyLinkedList(){
         size = 0;
     }
+
     public int size(){
         return size;
     }
+
     public boolean add(String value){
         if(size==0){
             start = new Node(value);
@@ -20,9 +22,27 @@ public class MyLinkedList{
         }
         return true;
     }
-    public boolean add(int index, String value);
+
+    public boolean add(int index, String value){
+        if(index<0||index>size){
+            throw new IndexOutOfBoundsException();
+        }
+        Node newValue = new Node(value);
+        Node place = start;
+
+    }
     public String get(int index);
     public String set(int index, String value);
     public String toString();
     //Any helper method that returns a Node object MUST BE PRIVATE!
+    
+    private Node getNode(int n){
+        if(n==0)return start;
+        if(n==size-1)return end;
+        Node curr = start;
+        for(int i=0;i<n;i++){
+            curr = curr.getNext();
+        }
+        return curr;
+    }
 }
